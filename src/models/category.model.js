059c74e2cec2c056b66bec037d55c6b394a73275
Abdelrahman Lila <abdelrahman.lila@ejust.edu.eg>
2026-07-12 15:31:53 +0300

    feat(validation-category): implement validation middlewares for categories

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -5,7 +5,8 @@ const categorySchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, "Category is required"],
-      unique: [true, "Category must be unique"],
+      unique: true,
+      trim: true,
       minlength: [3, "Minimum length for category is: 3"],
       maxlength: [32, "Maximum length for category is: 32"],
     },
