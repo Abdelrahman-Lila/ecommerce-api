289552bf13ed6777f75f5ea8d4768920d3c74bbd
Abdelrahman Lila <abdelrahman.lila@ejust.edu.eg>
2026-07-12 22:53:31 +0300

    feat(brands-crud): implement CRUD operations on brands

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ApiError from "./utils/api-error.js";
 import globalErrorHandler from "./middlewares/global-error-handler.middleware.js";
 import categoriesRouter from "./routes/category.route.js";
 import subCategoriesRouter from "./routes/subcategory.route.js";
+import brandsRouter from "./routes/brand.route.js";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,7 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/categories", categoriesRouter);
 app.use("/api/subcategories", subCategoriesRouter);
+app.use("/api/brands", brandsRouter);
 
 app.use((req, res, next) => {
   next(new ApiError(`This route is not found - "${req.originalUrl}"`, 404));
---
+++ src/routes/brand.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import * as brandController from "../controllers/brand.controller.js";
+import * as brandValidator from "../utils/validators/brand.validator.js";
+const router = express.Router();
+
+router
+  .route("/")
+  .get(brandController.getBrands)
+  .post(brandValidator.createBrandValidator, brandController.createBrand);
+
+router
+  .route("/:id")
+  .get(brandValidator.getBrandValidator, brandController.getBrand)
+  .put(brandValidator.updateBrandValidator, brandController.updateBrand)
+  .delete(brandValidator.deleteBrandValidator, brandController.deleteBrand);
+
+export default router;
